69eb61326e7f8aee89cd377698b745ee59c65150
duchi21 <spdgyjemp@gmail.com>
2024-07-25 16:04:43 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -9,7 +9,7 @@ power_xy <- function(a, b) {
 # Función recursiva para calcular (x^y)^z
 power_xyz <- function(a, b, c) {
   ab <- power_xy(a, b)
-  if (z == 0) {
+  if (c == 0) {
     return(1)
   } else {
     return(ab * power_xyz(a, b, c - 1))
